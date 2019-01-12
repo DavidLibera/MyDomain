5da0462942d7e8417a18c6ec6458cb5044124828
DavidLibera <david.libera@mail.mcgill.ca>
2019-01-12 13:51:46 -0500

    Completed requirements

--- routes.js
+++ routes.js
@@ -8,16 +8,15 @@ const requestHandler = (req,res) => {
 
         res.write('<html>');
         res.write('<body><header>First Application</header></body>');
-        res.write('<form action="/users" method="POST">'); // CHANGE DIRECTORY and METHOD is POST !!! sends POST request to /users
+        res.write('<form action="/create-users" method="POST">'); // CHANGE DIRECTORY and METHOD is POST !!! sends POST request to /users
         res.write('<input type="text" name="username">'); // SET key=username to input from user (i.e stream will look like username=David)
         res.write('<button type="submit">SEND</button>');
         res.write('</form>');
         res.write('</html>');
         return res.end();
-        console.log('my url is /');
     }
 
-    if( url === '/users' && method === 'POST'){
+    if( url === '/create-users' && method === 'POST'){
 
         // For acquiring username - PART 1
         const body = [];
@@ -28,34 +27,28 @@ const requestHandler = (req,res) => {
 
         // NOTE: One must return req.on() - because we do not want the event loop to proceed without executing this...
         return req.on('end',()=> {
-
             // For acquiring username - PART 2 
             const parsedBody = Buffer.concat(body).toString();
             const username = parsedBody.split("=")[1];
             console.log(username);
-
             // Adding user to users array
             users.push(username);
 
-            // Displaying users
-            res.getHeader('Content-Type','text/html');
-            res.write('<html>');
-            res.write('<body><header>List of users:</header></body>');
-
-            // Display users
-            console.log('users.length:' + users.length);
-            
-            //res.write('<script language="javascript" type="text/javascript">');
-            //res.write("<ul>");
-            //res.write('<li>' + username + '</li>');
-            //res.write("</ul>");
-            //res.write('</script>');
-
-            res.write('</html>');
+            res.setHeader('Location','/users');
             res.statusCode = 302;
             return res.end();
-
         });
+    }
+
+    if( url === '/users'){
+        // Displaying users
+        res.getHeader('Content-Type','text/html');
+        res.write('<html>');
+        res.write('<body><header>List of users:</header></body>');
+        res.write('<ul><li>User1</li><li>User2</li></ul>');
+        res.write('</html>');
+        res.statusCode = 302;
+        return res.end();
     }
 
     res.getHeader('Content-Type','text/html');
